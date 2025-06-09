014a665a67d403988cf25eb9c7ec62d12b2935d3
lisaifei <waltermitty121906@gmail.com>
2025-06-09 15:49:02 +0800

    format code

--- packages/vite-config/lib/withPageConfig.mjs
+++ packages/vite-config/lib/withPageConfig.mjs
@@ -19,25 +19,27 @@ export const watchOption = isDev ? {
  * @returns {UserConfig}
  */
 export function withPageConfig(config) {
-    return defineConfig(deepmerge(
-        {
-            base: '',
-            plugins: [react(), isDev && watchRebuildPlugin({ refresh: true })],
-            build: {
-                sourcemap: isDev,
-                minify: isProduction,
-                reportCompressedSize: isProduction,
-                emptyOutDir: isProduction,
-                watch: watchOption,
-                rollupOptions: {
-                    external: ['chrome'],
+    return defineConfig(
+        deepmerge(
+            {
+                base: '',
+                plugins: [react(), isDev && watchRebuildPlugin({ refresh: true })],
+                build: {
+                    sourcemap: isDev,
+                    minify: isProduction,
+                    reportCompressedSize: isProduction,
+                    emptyOutDir: isProduction,
+                    watch: watchOption,
+                    rollupOptions: {
+                        external: ['chrome'],
+                    },
                 },
+                define: {
+                    'process.env.NODE_ENV': isDev ? `"development"` : `"production"`,
+                },
+                envDir: '../..',
             },
-            define: {
-                'process.env.NODE_ENV': isDev ? `"development"` : `"production"`,
-            },
-            envDir: '../..',
-        },
-        config,
-    ));
+            config,
+        )
+    );
 }
